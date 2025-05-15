4c4edfa0a5ffdfd70ab326faf492654c7dfcbe0f
pengfei.lv <pengfei.lv@ucloud.cn>
2025-05-15 21:56:12 +0800

    test: sql

--- mysql/init/0_init.sql
+++ mysql/init/0_init.sql
@@ -3,7 +3,5 @@ CREATE DATABASE IF NOT EXISTS ucode_review;
 -- 设置数据库字符集和排序规则
 ALTER DATABASE ucode_review CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;  
 -- 为用户赋予权限
-DROP USER IF EXISTS 'root'@'%';
-CREATE USER 'root'@'%' IDENTIFIED BY 'mysql123456';
-GRANT ALL PRIVILEGES ON ucode_review.* TO 'root'@'%'; 
+GRANT ALL PRIVILEGES ON ucode_review.* TO 'mysql'@'%'; 
 FLUSH PRIVILEGES;
