c43782c2a8fdfa24f58210429f01ab0cfc940930
pengfei.lv <pengfei.lv@ucloud.cn>
2025-07-18 23:36:55 +0800

    fix: security

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,42 +1,42 @@
 import * as path from 'path';
 import { defineConfig, loadEnv, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-import { viteMockServe } from 'vite-plugin-mock';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+// import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const root = process.cwd();
 
   const env = loadEnv(mode, root);
-  const { VITE_APP_PORT, VITE_APP_MOCK } = env;
+  const { VITE_APP_PORT } = env;
 
-  const isBuild = command === 'build';
+  // const isBuild = command === 'build';
 
   // vite 插件
   const vitePlugins: (PluginOption | PluginOption[])[] = [
     react(),
     // vite-plugin-svg-icons
-    // createSvgIconsPlugin({
-      // // Specify the icon folder to be cached
-      // iconDirs: [path.resolve(__dirname, './src/assets/iconsvg')],
-      // // Specify symbolId format
-      // symbolId: 'icon-[name]',
-    // }),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(__dirname, './src/assets/iconsvg')],
+      // Specify symbolId format
+      symbolId: 'icon-[name]',
+    }),
   ];
   // vite-plugin-mock
-  if (VITE_APP_MOCK === 'true') {
-    vitePlugins.push(
-      viteMockServe({
-        mockPath: 'mock',
-        supportTs: true,
-        watchFiles: true,
-        localEnabled: !isBuild,
-        prodEnabled: isBuild,
-        logger: true,
-      }),
-    );
-  }
+  // if (VITE_APP_MOCK === 'true') {
+  //   vitePlugins.push(
+  //     viteMockServe({
+  //       mockPath: 'mock',
+  //       supportTs: true,
+  //       watchFiles: true,
+  //       localEnabled: !isBuild,
+  //       prodEnabled: isBuild,
+  //       logger: true,
+  //     }),
+  //   );
+  // }
 
   return {
     root,
